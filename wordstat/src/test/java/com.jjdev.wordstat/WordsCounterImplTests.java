ba5f21119f6433ac9e2132fd973d85f858dc91fb
Jakub <jakub.jankowski.dev@outlook.com>
2018-01-06 20:48:21 +0100

    fixing some weird problems with directories

--- wordstat/src/test/java/com.jjdev.wordstat/WordsCounterImplTests.java
+++ wordstat/src/test/java/com.jjdev.wordstat/WordsCounterImplTests.java
@@ -2,8 +2,6 @@ package com.jjdev.wordstat;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
 
 import java.util.Arrays;
 import java.util.List;
