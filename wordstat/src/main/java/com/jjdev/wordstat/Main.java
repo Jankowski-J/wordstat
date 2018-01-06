256563845a44227f2d99e18cfff22a33d06d6f66
Jakub <jakub.jankowski.dev@outlook.com>
2018-01-06 19:58:16 +0100

    first commit

---
+++ wordstat/src/main/java/com/jjdev/wordstat/Main.java
@@ -0,0 +1,11 @@
+package com.jjdev.wordstat;
+
+/**
+ *
+ * @author Jakub Jankowski
+ */
+public class Main {
+    public static void main(String args[]) {
+        FileParser parser = new FileParser();
+    }
+}
